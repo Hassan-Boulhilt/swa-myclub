8b91992c291339c60c89f9cc0e1ad4f424389dfc
hassan <hboulhilt@gmail.com>
2021-03-17 04:37:18 +0100

    Display dynamic content in home page 'Today date'.

--- events/views.py
+++ events/views.py
@@ -1,7 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from datetime import date
 
 
 
 def index(request):
-    return HttpResponse("<h1>MyClub Event Calender</h1>")
+    t = date.today()
+    month = date.strftime(t, '%b')
+    year = t.year
+    title = f"MyClub Event - {month}, {year}"
+    return HttpResponse(f'<h1>{title}</h1>')
