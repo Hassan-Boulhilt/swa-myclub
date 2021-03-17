dbdb6a93fed0960438e398b2a18a136158cb314d
hassan <hboulhilt@gmail.com>
2021-03-17 06:18:05 +0100

    display dynamicly specific date passed to URL or display default today date in view

--- events/views.py
+++ events/views.py
@@ -1,12 +1,20 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
 
+def index(request, month=date.today().month, year=date.today().year):
+    # t = date.today()
+    # month = date.strftime(t, '%b')
+    # year = t.year
 
-def index(request):
-    t = date.today()
-    month = date.strftime(t, '%b')
-    year = t.year
-    title = f"MyClub Event - {month}, {year}"
-    return HttpResponse(f'<h1>{title}</h1>')
+    month = int(month)
+    year = int(year)
+    if year < 2000 or year > 2099:
+        year = date.today().year
+    month_name = calendar.month_name[month]
+    cal = HTMLCalendar().formatmonth(year, month)
+    title = f"MyClub Event - {month_name}, {year}"
+    return HttpResponse(f'<h1>{title}{cal}</h1>')
