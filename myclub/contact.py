73389e910c9da46baba83ebb46d9dfce8a3a8965
hassan <hboulhilt@gmail.com>
2021-03-21 00:09:11 +0100

    add send mail backend to contact form

--- myclub/contact.py
+++ myclub/contact.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
 class ContactForm(forms.Form):
     yourname = forms.CharField(max_length=100, label='Your Name')
     email = forms.EmailField(required=False, label='Your Email Address')
@@ -16,6 +17,14 @@ def contact(request):
         if form.is_valid():
             cd = form.cleaned_data
             # assert False
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+                connection=con
+            )
             return HttpResponseRedirect('/contact?submitted=True')
     else:
         form = ContactForm()
