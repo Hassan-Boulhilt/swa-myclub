f455617a35a8b2ac80487dc565b9d8d5f31f2cc5
hassan <hboulhilt@gmail.com>
2021-04-04 16:07:29 +0100

    custom admin apps and model

--- events/admin.py
+++ events/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-
+from django.contrib.admin import AdminSite
+from django.contrib.auth.models import User, Group
 from .models import Venue, MyClubUser, Event
 
-@admin.register(Venue)
+class EventsAdmin(AdminSite):
+    site_header = "Swa-Club Administration"
+    site_title  = "Swa-Club Site Admin"
+    index_title = "Swa-Club Site Admin Home"
+admin_site = EventsAdmin(name='eventsadmin')
+@admin.register(Venue,site=admin_site)
 class VenueAdmin(admin.ModelAdmin):
     list_display =('name', 'address', 'phone')
     ordering = ('name',)
     search_fields = ('name','address')
 
-@admin.register(Event)
+@admin.register(Event, site=admin_site)
 class EventAdmin(admin.ModelAdmin):
     # fields = (('name', 'venue'), 'event_date', 'description', 'manager')
     list_display = ('name', 'event_date','venue' )
@@ -33,5 +39,7 @@ class EventAdmin(admin.ModelAdmin):
 # admin.site.register(Venue)
 admin.site.register(MyClubUser)
 # admin.site.register(Event)
+admin_site.register(User)
+admin_site.register(Group)
 
 
---
+++ myclub/admin.py
@@ -0,0 +1,8 @@
+
+from django.contrib.admin import AdminSite
+
+
+class MyClubAdmin(AdminSite):
+    site_header = "Swa-Club Administration"
+    site_title  = "Swa-Club Site Admin"
+    index_title = "Swa-Club Site Admin Home"
