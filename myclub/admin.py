e613ded087d0324c33a78747421e23c9fee5f8e9
hboulhilt <hboulhilt@gmail.com>
2021-04-07 14:35:21 +0100

    nc

--- myclub/admin.py
+++ myclub/admin.py
@@ -2,7 +2,7 @@
 from django.contrib.admin import AdminSite
 
 
-# class MyClubAdmin(AdminSite):
-#     site_header = "Swa-Club Administration"
-#     site_title  = "Swa-Club Site Admin"
-#     index_title = "Swa-Club Site Admin Home"
+class MyClubAdmin(AdminSite):
+    site_header = "Swa-Club Administration"
+    site_title  = "Swa-Club Site Admin"
+    index_title = "Swa-Club Site Admin Home"
